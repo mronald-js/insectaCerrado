8780922bd515fdd7f706df52b13b9d1d6e84b9ff
marcello.jsilva <marcello.jsilva@goias.gov.br>
2025-05-27 09:43:03 -0300

    initial template screen

---
+++ frontEnd/components/footer.tsx
@@ -0,0 +1,14 @@
+export default function Footer() {
+    return (
+        <footer className="bg-gray-800 text-white py-4">
+        <div className="container mx-auto text-center">
+            <p className="text-sm">
+            &copy; {new Date().getFullYear()} InsectaCerrado. All rights reserved.
+            </p>
+            <p className="text-xs mt-2">
+            Sistema de catalogação de insetos do Cerrado Brasileiro por INF - UFG
+            </p>
+        </div>
+        </footer>
+    );
+}
